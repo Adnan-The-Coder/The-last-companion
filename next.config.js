2263c8565cec7526c2591f9dad6a0140047e51cb
ArshilAliAbbas <arshil1227@gmail.com>
2025-05-22 09:17:32 -0400

    looks

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
+  // Other Next.js config options can go here
+};
+
+module.exports = nextConfig;
