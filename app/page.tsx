ae8334043fecf2d15a09899091b81835976e012e
Adnan <adnanali23189@gmail.com>
2025-05-22 11:01:23 +0530

    testing fork deployments

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div>This is content</div>
+      <div>This is content of Last Companion</div>
       <h1>Home Page is here</h1>
     </>
   );
